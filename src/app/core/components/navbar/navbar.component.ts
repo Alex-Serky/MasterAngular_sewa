f03ce8e078663873cc852292f46a77347f7d10c0
Alex Serky <aserky@gmail.com>
2021-10-18 19:13:02 +0200

    Composant de la barre de navigation

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'al-navbar',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  homePath: string = 'home';
+  loginPath: string = 'login';
+  registerPath: string = 'register';
 
-  ngOnInit(): void {
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {}
+
+  // Cette méthode permet de déterminer si la route passé en paramètre est la route courante ou non, sous la forme d'un booléen.
+  public isActive(page: string): boolean {
+    return this.router.isActive(page, true);
+  }
+
+  // Cette méthode permet de rediriger l'utilisateur vers une autre route.
+  public navigate(page: string): void {
+    this.router.navigate([page]);
   }
 
 }
