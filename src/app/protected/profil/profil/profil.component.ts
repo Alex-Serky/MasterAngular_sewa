ec55d37b438b6508c5f5f688bff332704a37128d
Alex Serky <aserky@gmail.com>
2021-10-28 15:16:57 +0200

    Ajout du formulaire d'édition du profil (MàJ)

--- src/app/protected/profil/profil/profil.component.ts
+++ src/app/protected/profil/profil/profil.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { User } from 'src/app/shared/models/user';
 
