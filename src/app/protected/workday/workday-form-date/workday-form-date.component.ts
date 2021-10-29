0cbddf16c8f6c95e97a8932f611fd13a2361646e
Alex Serky <aserky@gmail.com>
2021-10-29 15:43:02 +0200

    Intercepter le changement de journée

--- src/app/protected/workday/workday-form-date/workday-form-date.component.ts
+++ src/app/protected/workday/workday-form-date/workday-form-date.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { DateService } from 'src/app/core/services/date.service';
 
 @Component({
   selector: 'al-workday-form-date',
@@ -12,10 +13,21 @@ export class WorkdayFormDateComponent implements OnInit {
 
   @Input() dueDate: FormControl;
 
-  constructor(private localeService: BsLocaleService) { }
+  constructor(
+    private localeService: BsLocaleService,
+    private dateService: DateService) { }
 
   ngOnInit(): void {
     this.localeService.use('fr');
+  }
+
+  // Son rôle est d'intercepter les dates via bsValueChange
+  selectDate(date: Date): void {
+    if(date) {
+      // Transformer en sa date d'affichage grâce à la méthode getDisplayDate
+      const displayDate: string = this.dateService.getDisplayDate(date);
+      this.dateSelected.emit(displayDate);
+    }
   }
 
 }
